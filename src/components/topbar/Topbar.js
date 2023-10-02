5b7e04007805c842befcfbbb58efae7449d024ca
Ossiakeme Stephen <stephenossiakeme@MaestroMacBook-Pro.local>
2023-10-02 08:40:19 +0100

    feat:add H1 tag to topbar

---
+++ src/components/topbar/Topbar.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './topbar.css'
+
+function Topbar() {
+  return ( <div>
+    <div>topbar, is active.</div>
+    <div>
+      <h1> this is for all.</h1>
+    </div>
+    </div>
+    
+  )
+}
+
+export default Topbar;
