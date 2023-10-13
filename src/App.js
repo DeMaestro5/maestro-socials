80b061cd24076835b8076baee32ab9f6ee552a75
Ossiakeme Stephen <stephenossiakeme@MaestroMacBook-Pro.local>
2023-10-13 09:10:36 +0100

    feat: added the profile page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from './Pages/home/Home';
+import Profile from './Pages/profile/Profile';
 
 
 function App() {
   return (
-    <Home />
+    <Profile />
   )
 }
 
--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -3,10 +3,11 @@ import './rightbar.css'
 import {Users} from '../../dummyData'
 import Online from '../online/Online'
 
-function Rightbar() {
-  return (
-    <div className='rightbar'>
-      <div className="rightbarWrapper">
+function Rightbar({profile}) {
+
+  const HomeRightbar = () =>{
+    return (
+      <>
       <div className="birthdayContainer">
       <img src="assets/gift.png" alt="" className="birthdayImg" />
       <span className="birthdayText"><b>Big Ben</b> and <b>5 others</b> have a birthday today</span>
@@ -18,6 +19,62 @@ function Rightbar() {
         <Online key={u.id} user={u}/>
        ))}
        </ul>
+      </>
+    )
+  }
+
+  const ProfileRightbar = () => {
+    return(
+      <>
+      <h4 className="rightbarTitle">User Information</h4>
+      <div className="rightbarInfo">
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">City:</span>
+          <span className="rightbarInfoValue">Asaba</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">From:</span>
+          <span className="rightbarInfoValue">Delta</span>
+        </div>
+        <div className="rightbarInfoItem">
+          <span className="rightbarInfoKey">Relationship:</span>
+          <span className="rightbarInfoValue">Single</span>
+        </div>
+      </div>
+      <h4 className="rightbarTitle">User Friends</h4>
+      <div className="rightbarFollowings">
+        <div className="rightbarFollowing">
+          <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Mason Showman</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Kathy Pierce</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/3.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">David Madison</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Jerry Freeman</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Morgan Robin</span>
+        </div>
+        <div className="rightbarFollowing">
+          <img src="assets/person/6.jpeg" alt="" className="rightbarFollowingImg" />
+          <span className="rightbarFollowingName">Patrick Wickmann</span>
+        </div>
+      </div>
+      </>
+    )
+  }
+  return (
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+      <ProfileRightbar/>
       </div>
       </div>
   )
